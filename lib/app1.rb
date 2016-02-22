bcfb8f434b37863b2b91fa52b0a4ea02a6cbd278
dujar <dujar.coding@gmail.com>
2016-02-22 13:32:59 +0100

    brands section finished with polished with minor edit for corrections of toy section

--- lib/app1.rb
+++ lib/app1.rb
@@ -82,6 +82,7 @@ puts testing "ho",hi:"ploen"
                         end
                     arr1 = arr.inject(:+)
                     arr2 = arr2.push arr1
+                    arr=[]
                     end
                     return arr2
 
@@ -103,8 +104,10 @@ puts testing "ho",hi:"ploen"
                         end
                     arr1 = arr.inject(:+)
                     arr2 = arr2.push arr1/arr.length
+                    arr = []
                     end
                     return arr2
+
                 elsif options[:purchases]=="yes" && options[:discount]
 
                     averaged = item_array item: "price", purchases: "yes", average: "yes"
@@ -119,7 +122,7 @@ puts testing "ho",hi:"ploen"
 
 
 
-                elsif options[:purchases]
+                elsif options[:purchases] && options[:item]
                     purch = []
                     $products_hash["items"].each do |purcha|
                         purch = purch.push purcha["purchases"]
@@ -163,10 +166,50 @@ puts item_array item: "price", purchases: "yes", discount: "yes"
 
 puts a.asciify('Brands')
 
+#methods gathering information for Brand
+    def brand_array(options = {})
+                    lego = []
+                    nano =[]
+                    $products_hash["items"].each do |brand|
+                        
+                        if brand["brand"]== "LEGO"
+                            lego = lego.push brand[options[:item]].to_f
+                        else
+                            nano = nano.push brand[options[:item]].to_f
+                        end
+                    end
+
+                if options[:item] && options[:stock]=="yes"
+                    
+                    lego_sum = lego.inject(:+)
+                    nano_sum = nano.inject(:+)
+                    brand_sum = [LegoStock: lego_sum, NanoStock: nano_sum]
+                    return brand_sum
+                elsif options[:average]=="yes" && options[:item]
+
+                    lego_sum = lego.inject(:+)
+                    nano_sum = nano.inject(:+)
+                    lego_price = lego_sum/lego.length
+                    nano_price = nano_sum/nano.length
+                    brand_sum = [LegoMeanPrice: lego_price.round(2), NanoMeanPrice: nano_price.round(2)]
+                    return brand_sum
+                else
+                   puts "are you sure you did not forget any options?"
+                    
+                end
+    end
 # For each brand in the data set:
 	# Print the name of the brand
+    puts "the different brands are:"
+    puts (item_array item: "brand").uniq
 	# Count and print the number of the brand's toys we stock
+    puts "the amount of stocks each brand has are:"
+    puts brand_array item: "stock", stock: "yes"
 	# Calculate and print the average price of the brand's toys
+    puts "the average price of the brand's toys are:"
+    puts brand_array item: "full-price", average: "yes"
 	# Calculate and print the total sales volume of all the brand's toys combined
+    puts "the sum of all the toys combined are:"
+    puts (item_array item: "price", purchases: "yes", sum: "yes").inject(:+).round(2)
 
 
