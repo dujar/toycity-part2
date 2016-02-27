5b2994984b77a1459b63fa5c16494b17a15ceb05
dujar <dujar.coding@gmail.com>
2016-02-27 15:37:40 +0100

    naked project since will start from beginning

--- lib/app.rb
+++ lib/app.rb
@@ -46,166 +46,20 @@ puts a.asciify('Products')
 	# Calculate and print the average discount (zo% or $) based off the average sales price
 
 #methods gathering information for toy
-
-#methods gathering information for toy
-    def item_array(options = {})
-                if options[:purchases]=="yes" && options[:count]=="yes"
-                    
-                    purch = []
-                    $products_hash["items"].each do |purcha|
-                    purch = purch.push purcha["purchases"].count
-                    end
-                    arr = purch
-                    return arr
-
-
-
-                elsif options[:purchases]=="yes" && options[:sum]=="yes"
-                     
-                    purch1 = []
-                    purch = []
-                    $products_hash["items"].each do |purcha|
-                        purch = purch.push purcha["purchases"]
-                        purch1 = purch1.push purcha["purchases"].count
-                    end
-
-                    arr =[]
-                    arr1 = []
-                    arr2 = []
-                    purch1.each_index do |index|
-                        purch[index].each do |toy|
-                        arr = arr.push toy[options[:item]]
-                        end
-                    arr1 = arr.inject(:+)
-                    arr2 = arr2.push arr1
-                    arr=[]
-                    end
-                    return arr2
-
-                elsif options[:purchases]=="yes" && options[:average]=="yes"
-                     
-                    purch1 = []
-                    purch = []
-                    $products_hash["items"].each do |purcha|
-                        purch = purch.push purcha["purchases"]
-                        purch1 = purch1.push purcha["purchases"].count
-                    end
-
-                    arr =[]
-                    arr1 = []
-                    arr2 = []
-                    purch1.each_index do |index|
-                        purch[index].each do |toy|
-                        arr = arr.push toy[options[:item]]
-                        end
-                    arr1 = arr.inject(:+)
-                    arr2 = arr2.push arr1/arr.length
-                    arr = []
-                    end
-                    return arr2
-
-                elsif options[:purchases]=="yes" && options[:discount]
-
-                    averaged = item_array item: "price", purchases: "yes", average: "yes"
-                    full_price = item_array item: "full-price"
-                    discounted = []
-                    full_price.each_index do |i|
-                        discounted = discounted.push (full_price[i].to_f-averaged[i]).round(2)
-                    end
-                    
-                    return discounted
-                        
-
-
-
-                elsif options[:purchases] && options[:item]
-                    purch = []
-                    $products_hash["items"].each do |purcha|
-                        purch = purch.push purcha["purchases"]
-                    end
-                    arr =[]
-                    purch.each do |toy|
-                        arr = arr.push toy[options[:item]]
-                    end
-
-                    return arr
-                
-                
-                else
-                    arr =[]
-                    $products_hash["items"].each do |toy|
-                        arr = arr.push toy[options[:item]]
-                    end
-                    return arr
-                end
-    end
 # Print the name of the toy
-puts "the name of the toys are:"
-puts item_array item: "title"
 	# Calculate and print the total number of purchases
-puts "the number of purchases of each toy are:"
-puts item_array item: "", count: "yes", purchases: "yes"
 	# Print the retail price of the toy
-puts "the retail price of each toy are"
-puts item_array item: "full-price"
 	# Calculate and print the total amount of sales
-puts "the total amount of sales for each toy are:"
-puts item_array item: "price", purchases: "yes", sum: "yes"
 	# Calculate and print the average price the toy sold for
-puts "the average price the toy sold for are:"
-puts item_array item: "price", purchases: "yes", average: "yes"
 	# Calculate and print the average discount (% or $) based off the average sales price
-puts "the average discount in $ are:"
-puts item_array item: "price", purchases: "yes", discount: "yes"
 
 # Print "Brands" in ascii art
 
 puts a.asciify('Brands')
 
 #methods gathering information for Brand
-    def brand_array(options = {})
-                    lego = []
-                    nano =[]
-                    $products_hash["items"].each do |brand|
-                        
-                        if brand["brand"]== "LEGO"
-                            lego = lego.push brand[options[:item]].to_f
-                        else
-                            nano = nano.push brand[options[:item]].to_f
-                        end
-                    end
-
-                if options[:item] && options[:stock]=="yes"
-                    
-                    lego_sum = lego.inject(:+)
-                    nano_sum = nano.inject(:+)
-                    brand_sum = [LegoStock: lego_sum, NanoStock: nano_sum]
-                    return brand_sum
-                elsif options[:average]=="yes" && options[:item]
-
-                    lego_sum = lego.inject(:+)
-                    nano_sum = nano.inject(:+)
-                    lego_price = lego_sum/lego.length
-                    nano_price = nano_sum/nano.length
-                    brand_sum = [LegoMeanPrice: lego_price.round(2), NanoMeanPrice: nano_price.round(2)]
-                    return brand_sum
-                else
-                   puts "are you sure you did not forget any options?"
-                    
-                end
-    end
-# For each brand in the data set:
-	# Print the name of the brand
-    puts "the different brands are:"
-    puts (item_array item: "brand").uniq
 	# Count and print the number of the brand's toys we stock
-    puts "the amount of stocks each brand has are:"
-    puts brand_array item: "stock", stock: "yes"
 	# Calculate and print the average price of the brand's toys
-    puts "the average price of the brand's toys are:"
-    puts brand_array item: "full-price", average: "yes"
 	# Calculate and print the total sales volume of all the brand's toys combined
-    puts "the sum of all the toys combined are:"
-    puts (item_array item: "price", purchases: "yes", sum: "yes").inject(:+).round(2)
 
 
